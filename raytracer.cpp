e73d1a978bec691963156818e084b9f57b6ff314
gcem <34423323+gcem@users.noreply.github.com>
2020-11-14 18:23:55 +0300

    Change code to render the time for each pixel

--- raytracer.cpp
+++ raytracer.cpp
@@ -17,16 +17,35 @@ int main(int argc, char *argv[]) {
     scene.loadFromXml(argv[1]);
 
     for (auto camera : scene.cameras) {
+        // TODO: delete
+        int64_t *times = new int64_t[camera.width * camera.height];
+        int64_t maxTime = 0;
+
         unsigned char *image =
             new unsigned char[camera.width * camera.height * 3];
         std::vector<Ray> rays;
         castRays(rays, camera);
         for (int i = 0, k = 0; i < camera.width * camera.height; i++) {
+            auto before = std::chrono::system_clock::now();
             auto color = trace(rays[i], scene.max_recursion_depth, scene);
+            auto after = std::chrono::system_clock::now();
+            times[i] = (after - before).count();
+            maxTime = std::max(maxTime, times[i]);
+
+            k++;
+            image[k++] = 0;
+            image[k++] = 0;
+            /*
             image[k++] = color.x > 255 ? 255 : std::roundf(color.x);
             image[k++] = color.y > 255 ? 255 : std::roundf(color.y);
             image[k++] = color.z > 255 ? 255 : std::roundf(color.z);
+            */
         }
+
+        for (int i = 0; i < camera.width * camera.height; i++) {
+            image[i * 3] = times[i] * 255.f / maxTime;
+        }
+
         write_ppm("output/" + camera.image_name, image, camera.width,
                   camera.height);
         delete[] image;
