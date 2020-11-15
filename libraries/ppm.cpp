22e8c8bb1c1412cbd289e088507acb40936933c2
gcem <34423323+gcem@users.noreply.github.com>
2020-11-15 13:05:20 +0300

    Fix the bug, add a useless alignment

--- libraries/ppm.cpp
+++ libraries/ppm.cpp
@@ -6,40 +6,39 @@
 
 void writeToBuffer(std::string *out, uint8_t *data, int start, int end,
                    int width) {
-
     out->reserve(width * (end - start) * 3 * 4);
     std::stringstream str;
     for (int row = start; row < end; row++) {
         int coli = row * width * 3;
         for (; coli < (row + 1) * width * 3 - 1; coli++) {
-            str << (short)data[coli] << ' ';
+            str << (int)data[coli] << ' ';
         }
-        str << (short)data[coli] << '\n';
+        str << (int)data[coli] << '\n';
         out->append(str.str());
         str.str(std::string());
         str.clear();
     }
 }
 
-void write_ppm(std::string filename, uint8_t *data, int width,
-               int height) {
+void write_ppm(std::string filename, uint8_t *data, int width, int height) {
     std::ofstream outf;
     outf.open(filename, std::ofstream::out | std::ofstream::trunc);
 
     outf << "P3\n" << width << ' ' << height << "\n255\n";
 
     int part = (height + THREAD_CNT - 1) / THREAD_CNT;
-    std::string buffers[8];
+
+    std::string buffers[THREAD_CNT * 2]; // align to 64 bytes
     std::thread bufferWriters[8];
 
     for (int i = 0; i < THREAD_CNT; i++) {
         bufferWriters[i] =
-            std::thread(writeToBuffer, buffers + i, data, i * part,
+            std::thread(writeToBuffer, buffers + (i << 1), data, i * part,
                         std::min((i + 1) * part, height), width);
     }
     for (int i = 0; i < THREAD_CNT; i++) {
         bufferWriters[i].join();
-        outf << buffers[i];
+        outf << buffers[i << 1];
     }
 
     outf.close();
