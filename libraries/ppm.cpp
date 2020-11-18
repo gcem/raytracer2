c849b7f85c52746664aff438581d60d71e6d0535
gcem <34423323+gcem@users.noreply.github.com>
2020-11-18 16:51:13 +0300

    Make first thread's output shorter in ppm

--- libraries/ppm.cpp
+++ libraries/ppm.cpp
@@ -29,14 +29,23 @@ void write_ppm(std::string filename, uint8_t **data, int width, int height) {
 
     int part = (height + THREAD_CNT - 1) / THREAD_CNT;
 
-    std::string buffers[THREAD_CNT * 2]; // align to 64 bytes
+    std::string buffers[THREAD_CNT * 2 - 1]; // align to 64 bytes
     std::thread bufferWriters[THREAD_CNT];
 
-    for (int i = 0; i < THREAD_CNT; i++) {
+    bufferWriters[0] = std::thread(writeToBuffer, buffers, data, 0,
+                                   std::min((part + 1) / 2, height), width);
+
+    for (int i = 1; i < THREAD_CNT - 1; i++) {
         bufferWriters[i] =
-            std::thread(writeToBuffer, buffers + (i << 1), data, i * part,
-                        std::min((i + 1) * part, height), width);
+            std::thread(writeToBuffer, buffers + (i << 1), data,
+                        (i - 1) * part + (part + 1) / 2,
+                        std::min(i * part + (part + 1) / 2, height), width);
     }
+
+    bufferWriters[THREAD_CNT - 1] =
+        std::thread(writeToBuffer, buffers + (THREAD_CNT * 2 - 2), data,
+                    std::min((THREAD_CNT - 2) * part + (part + 1) / 2, height), height, width);
+
     for (int i = 0; i < THREAD_CNT; i++) {
         bufferWriters[i].join();
         outf << buffers[i << 1];
