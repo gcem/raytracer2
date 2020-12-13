fc3cf11b080b956ce5966e56aacaf9340aa16759
gcem <34423323+gcem@users.noreply.github.com>
2020-12-14 02:03:21 +0300

    Disable sphere direction check to render skybox correctly (allow camera inside sphere)

--- surfaces/sphere.cpp
+++ surfaces/sphere.cpp
@@ -6,8 +6,10 @@ Sphere::Sphere(const Vec3f &center, float radius, int materialId)
 
 float Sphere::intersect(const Ray &ray, Ray &normalOut) {
     auto centerPos = center - ray.origin;
+    /* cameras can be inside spheres now
     if (centerPos.dot(ray.direction) < 0)
         return -1;
+    */
     float centerDist = centerPos.norm();
     auto centerDirection = centerPos * (1 / centerDist);
     float perpRadius = ray.direction.sine(centerDirection) * centerDist;
@@ -16,6 +18,9 @@ float Sphere::intersect(const Ray &ray, Ray &normalOut) {
 
     float t = centerDirection.dot(ray.direction) * centerDist -
               sqrt(radius * radius - perpRadius * perpRadius);
+    if (t < 0) // camera inside sphere
+        t = centerDirection.dot(ray.direction) * centerDist +
+            sqrt(radius * radius - perpRadius * perpRadius);
 
     normalOut.origin = ray.origin + ray.direction * t;
     normalOut.direction = (normalOut.origin - center) * (1 / radius);
@@ -25,9 +30,11 @@ float Sphere::intersect(const Ray &ray, Ray &normalOut) {
 
 bool Sphere::intersectsBefore(const Ray &ray, float maxt) {
     auto centerPos = center - ray.origin;
+    float centerDist = centerPos.norm();
+    if (centerDist < radius - 0.0001f) // we are inside the sphere
+        return false; // no shadow inside spheres. whatever.
     if (centerPos.dot(ray.direction) < 0)
         return false;
-    float centerDist = centerPos.norm();
     auto centerDirection = centerPos * (1 / centerDist);
     float perpRadius = ray.direction.sine(centerDirection) * centerDist;
     if (perpRadius > radius)
@@ -48,8 +55,10 @@ TexturedSphere::TexturedSphere(const Vec3f &center, float radius,
 float TexturedSphere::intersect(const Ray &ray, Ray &normalOut,
                                 Vec2f &texCoordOut) {
     auto centerPos = center - ray.origin;
+    /* cameras can be inside spheres now
     if (centerPos.dot(ray.direction) < 0)
         return -1;
+    */
     float centerDist = centerPos.norm();
     auto centerDirection = centerPos * (1 / centerDist);
     float perpRadius = ray.direction.sine(centerDirection) * centerDist;
@@ -58,6 +67,9 @@ float TexturedSphere::intersect(const Ray &ray, Ray &normalOut,
 
     float t = centerDirection.dot(ray.direction) * centerDist -
               sqrt(radius * radius - perpRadius * perpRadius);
+    if (t < 0) // camera inside sphere
+        t = centerDirection.dot(ray.direction) * centerDist +
+                   sqrt(radius * radius - perpRadius * perpRadius);
 
     normalOut.origin = ray.origin + ray.direction * t;
     normalOut.direction = (normalOut.origin - center) * (1 / radius);
