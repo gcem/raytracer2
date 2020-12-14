b84314dd1dbfbb4bc5e48fef61a1d9da47626de6
gcem <34423323+gcem@users.noreply.github.com>
2020-12-14 18:16:38 +0300

    Orthogonalize camera vectors

--- ray.cpp
+++ ray.cpp
@@ -1,16 +1,20 @@
 #include "ray.h"
 
 void castRays(Ray *out, const Camera &camera) {
-    Vec3f right = camera.gaze.cross(camera.up).normalize();
-    Vec3f up = camera.up.normalize();
-    Vec3f leftdir = camera.gaze * camera.near_distance +
+    // orthogonalize, since that requirement also seems to have changed in
+    // THE2
+    Vec3f gaze = camera.gaze.normalize();
+    Vec3f right = gaze.cross(camera.up).normalize();
+    Vec3f up = right.cross(camera.gaze).normalize();
+
+    Vec3f leftdir = gaze * camera.near_distance +
                     right * camera.near_plane.left +
                     up * camera.near_plane.top;
     Vec3f currdir;
     right = right * ((camera.near_plane.right - camera.near_plane.left) /
                      (camera.width));
     up = up * ((camera.near_plane.top - camera.near_plane.bottom) /
-                   (camera.height));
+               (camera.height));
     leftdir = leftdir + right * 0.5 - up * 0.5;
 
     int i = 0;
